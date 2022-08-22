051286304aa74947d1e37aaec0057dcf8b345e15
Sol <solemich@gmail.com>
2022-08-22 17:25:03 -0400

    tabla

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,86 @@
+import Papa from "papaparse"
+import * as XLSX from "xlsx"
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import DataTable from "react-data-table-component"
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const Loader = () => {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <h3>Cargando BHP Payroll...</h3>
     </div>
   )
+}
+
+
+const App = () => {
+
+  const [parsedData, setParsedData] = useState([])
+  const [columns, setColumns] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  const readFile = () => {
+
+    const e = document.getElementById("file")
+    const [file] = e.files
+    if(!file){
+      console.log("nope")
+      return
+    }
+
+    setLoading(true)
+
+    const reader = new FileReader()
+
+    reader.onload = (evt) => {
+      const bstr =evt.target.result  
+      const wb = XLSX.read(bstr, {type: "binary"})
+      const wsname =wb.SheetNames[0]
+      const ws = wb.Sheets[wsname]
+      const data = XLSX.utils.sheet_to_csv(ws, { header: 1})
+      parseFile(data)
+    }
+    reader.readAsBinaryString(file)
+  }
+
+  const createColumns = (data) => {
+    const columns = []
+    data.forEach((item) => {
+      columns.push({
+        name: item,
+        selector: row =>row[item],
+        sortable: true
+      })
+      
+    })
+    setColumns(columns)
+    
+  }
+
+  const parseFile = (data) => {
+    Papa.parse(
+      data,
+      {
+        header: true,
+        complete: (result)=> {
+          createColumns(Object.keys(result.data[0]))
+          setParsedData(result.data)
+
+          setTimeout(() => {
+            setLoading(false)
+          }, 2000)
+        }
+      }
+    )
+  }
+  return (
+    <div className="App">
+      <input type="file" id="file" name="file"></input>
+      <button onClick={readFile}>Cargar</button>
+      { loading && <Loader />}
+      { !loading && columns.length > 0 && parsedData.length > 0 && <DataTable columns={columns} data={parsedData} pagination /> }
+    </div>
+)
 }
 
 export default App
